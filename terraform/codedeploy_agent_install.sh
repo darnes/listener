1db9cf499066d33f3685bb0c6ae8f7336a0ce225
dimas <darnesmeister@gmail.com>
2022-02-16 05:58:23 +1100

    allow ssh connect and agent install fix

--- terraform/codedeploy_agent_install.sh
+++ terraform/codedeploy_agent_install.sh
@@ -2,7 +2,7 @@
 sudo apt-get update -y
 sudo apt-get install ruby -y
 sudo apt-get install wget -y
-cd /home/ubuntu
+cd /home/ec2-user
 wget https://aws-codedeploy-us-east-1.s3.amazonaws.com/latest/install
 chmod +x ./install
 sudo ./install auto
--- terraform/main.tf
+++ terraform/main.tf
@@ -15,13 +15,37 @@ provider "aws" {
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-038b3df3312ddf25d"
+  ami = "ami-038b3df3312ddf25d"
   instance_type = "t2.medium"
-  
+  vpc_security_group_ids = [aws_security_group.listener-sg.id]
+
   user_data = file("codedeploy_agent_install.sh")
   
   tags = {
-    Name = "ExampleAppServerInstance"
+    Name = "ListenerInstance"
+  }
+  key_name = "MasterKey"
+}
+
+resource "aws_security_group" "listener-sg" {
+  name = "listener-sg"
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
+
+# resource "aws_key_pair" "master_key" {
+#   key_name   = "deployer-key"
+#   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQD3F6tyPEFEzV0LX3X8BsXdMsQz1x2cEikKDEY0aIj41qgxMCP/iteneqXSIFZBp5vizPvaoIR3Um9xK7PGoW8giupGn+EPuxIA4cDM4vzOqOkiMPhz5XK0whEjkVzTo4+S0puvDZuwIsdiW9mxhJc7tgBNL0cYlWSYVkz4G/fslNfRPW5mYAM49f4fhtxPb5ok4Q2Lg9dPKVHO/Bgeu5woMc7RY0p1ej6D4CKFE6lymSDJpW0YHX/wqE9+cfEauh7xZcG0q9t2ta6F6fmX0agvpFyZo8aFbXeUBr7osSCJNgvavWbM/06niWrOvYX2xwWdhXmXSrbX8ZbabVohBK41 email@example.com"
+# }
