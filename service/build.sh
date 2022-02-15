43eb3224ccec998ebc40d5d6c6c3fcb6640f0608
dimas <darnesmeister@gmail.com>
2022-02-15 17:54:53 +1100

    add appspec

--- service/build.sh
+++ service/build.sh
@@ -3,4 +3,6 @@
 mkdir -p ./build
 rm -rf ./build/*
 pwd
-tar -zcvf ./build/artifact.tar.gz src/
+# tar -zcvf ./build/artifact.tar.gz src/
+cp -r ./src/* ./build
+cp ./appspec.yml ./build/appspec.yml
