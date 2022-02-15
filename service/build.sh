e58c595665567dcd291a6f547f82e3c8509180a5
dimas <darnesmeister@gmail.com>
2022-02-15 18:05:47 +1100

    correct artifact

--- service/build.sh
+++ service/build.sh
@@ -3,6 +3,6 @@
 mkdir -p ./build
 rm -rf ./build/*
 pwd
-# tar -zcvf ./build/artifact.tar.gz src/
-cp -r ./src/* ./build
+tar -zcvf ./build/artifact.tar.gz src/
+# cp -r ./src/* ./build
 cp ./appspec.yml ./build/appspec.yml
