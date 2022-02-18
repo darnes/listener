bd6d60556eb62058af714bb144693ac102c9a735
dimas <darnesmeister@gmail.com>
2022-02-19 09:40:54 +1100

    deploy debug

--- service/hooks/after-install.sh
+++ service/hooks/after-install.sh
@@ -3,8 +3,12 @@
 mkdir -p /home/ec2-user/listener-app
 pwd
 tar -xzvf /home/ec2-user/artifact.tar.gz -C /home/ec2-user/listener-app
+
 echo 'unpack complete, refreshing supervisor config'
+
+#sudo chmod a+x /home/ec2-user/listener-app/src/start.sh # no need 
 
 sudo mv /home/ec2-user/listener.ini /etc/supervisord.d/
 sudo supervisorctl reread
 sudo supervisorctl update
+sudo supervisorctl restart listener
--- src/main.py
+++ src/main.py
@@ -8,12 +8,15 @@ log = logging.getLogger(__name__)
 
 def main():
     log.info('starting')
+    countdown = 10
     while True:
         time.sleep(1.0)
         log.info('info beep')
         log.debug('debug beep')
         log.warning('warning beep')
         log.error('error beep')
+        countdown -= 1
+        log.info('1/countdown %s', 1 / countdown)
 
 if __name__ == '__main__':
     main()
