77041626708dcfefe4163fc0affa277e771843ec
dimas <darnesmeister@gmail.com>
2022-02-19 09:22:55 +1100

    deploy debug

--- service/hooks/after-install.sh
+++ service/hooks/after-install.sh
@@ -3,4 +3,8 @@
 mkdir -p /home/ec2-user/listener-app
 pwd
 tar -xzvf /home/ec2-user/artifact.tar.gz -C /home/ec2-user/listener-app
-echo "unpack complete"
+echo 'unpack complete, refreshing supervisor config'
+
+sudo mv /home/ec2-user/listener.ini /etc/supervisord.d/
+sudo supervisorctl reread
+sudo supervisorctl update
