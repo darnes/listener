7db972d9972607e0d5c3853f5773497b84edadb5
dimas <darnesmeister@gmail.com>
2022-03-05 09:01:06 +1100

    minor comments

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ Simple OandaClient that reads data (eventually)
 Zero console app. 
 Minimum functionality includes:
 
+TBD:
 * test
 * codestyle check
 * log to cloudWatch
--- terraform/main.tf
+++ terraform/main.tf
@@ -56,3 +56,31 @@ resource "aws_security_group" "listener-sg" {
 #   key_name   = "deployer-key"
 #   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQD3F6tyPEFEzV0LX3X8BsXdMsQz1x2cEikKDEY0aIj41qgxMCP/iteneqXSIFZBp5vizPvaoIR3Um9xK7PGoW8giupGn+EPuxIA4cDM4vzOqOkiMPhz5XK0whEjkVzTo4+S0puvDZuwIsdiW9mxhJc7tgBNL0cYlWSYVkz4G/fslNfRPW5mYAM49f4fhtxPb5ok4Q2Lg9dPKVHO/Bgeu5woMc7RY0p1ej6D4CKFE6lymSDJpW0YHX/wqE9+cfEauh7xZcG0q9t2ta6F6fmX0agvpFyZo8aFbXeUBr7osSCJNgvavWbM/06niWrOvYX2xwWdhXmXSrbX8ZbabVohBK41 email@example.com"
 # }
+
+#TBD: make output with DNS name
+# cloudwatch logs perms:
+# {
+#   "Version": "2012-10-17",
+#   "Statement": [
+#     {
+#       "Action": [
+#         "logs:Create*",
+#         "logs:PutLogEvents"
+#         ],
+#       "Effect": "Allow",
+#       "Resource": "arn:aws:logs:*:*:*"
+#     },
+#     {
+#       "Action": [
+#         "ecs:CreateCluster",
+#         "ecs:DeregisterContainerInstance",
+#         "ecs:DiscoverPollEndpoint",
+#         "ecs:RegisterContainerInstance",
+#         "ecs:Submit*",
+#         "ecs:Poll"
+#       ],
+#       "Effect": "Allow",
+#       "Resource": "*"
+#     }
+#   ]
+# }
